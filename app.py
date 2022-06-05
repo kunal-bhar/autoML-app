c3c26e884e08e70f303e88f08d27db51bf18ea6a
kunal-bhar <kunal21102@gmail.com>
2022-06-05 20:28:59 +0530

    Code Review #3:  Updated Loc, Page Config and Markdown

--- app.py
+++ app.py
@@ -11,23 +11,23 @@ import base64
 import io
 
 # Layout
-st.set_page_config(page_title= 'Automating Machine Learning', layout= 'wide')
+st.set_page_config(page_title= 'AutoML', layout= 'wide', page_icon= '📈')
 
 # Model Building
 def build_model(df):
   X= df.iloc[:, :-1] 
   Y= df.iloc[:, -1]
   
-  st.markdown('**1.2 Dataset Shape')
-  st.write('X')
+  st.markdown('**1.2 Shape**')
+  st.write('Independent Variables aka/ X')
   st.info(X.shape)
-  st.write('Y')
+  st.write('Dependent Variable aka/ Y')
   st.info(Y.shape)
   
-  st.markdown('**1.3 Variable Details')
-  st.write('X variable (shown upto 20 values)')
+  st.markdown('**1.3 Variables**')
+  st.write('X (shown upto 20 values)')
   st.info(list(X.columns[:20]))
-  st.write('Y variable')
+  st.write('Y')
   st.info(Y.name)
   
   X_train, X_test, Y_train, Y_test= train_test_split(X, Y, test_size= split_size, random_state= seed_number)
@@ -39,11 +39,11 @@ def build_model(df):
   
   st.write('Training Set')
   st.write(predictions_train)
-  st.markdown(filedownload(predictions_train, 'training.csv'), unsafe_allow_html= True)
+  st.markdown(filedownload(predictions_train, 'Training Metrics'), unsafe_allow_html= True)
   
   st.write('Test Set')
   st.write(predictions_test)
-  st.markdown(filedownload(predictions_test, 'test.csv'), unsafe_allow_html= True)
+  st.markdown(filedownload(predictions_test, 'Test Metrics'), unsafe_allow_html= True)
   
   st.subheader('3. Plots @ Model Performance')
   
@@ -55,7 +55,7 @@ def build_model(df):
     sns.set_theme(style= 'whitegrid')
     ax1= sns.barplot(y= predictions_test.index, x= 'R-Squared', data= predictions_test)
     ax1.set(xlim= (0, 1))
-  st.markdown(imagedownload(plt, 'plot-r2-tall.pdf'), unsafe_allow_html= True)
+  st.markdown(imagedownload(plt, 'R^2 Plot [Portrait]'), unsafe_allow_html= True)
   
     # Landscape Plot
   plt.figure(figsize= (9, 3))
@@ -64,16 +64,16 @@ def build_model(df):
   ax1.set(ylim= (0, 1))
   plt.xticks(rotation= 90)
   st.pyplot(plt)
-  st.markdown(imagedownload(plt, 'plot-r2-wide.pdf'), unsafe_allow_html= True) 
+  st.markdown(imagedownload(plt, 'R^2 Plot [Landscape]'), unsafe_allow_html= True) 
     
-  with st.markdown('**RMSE**'):
+  with st.markdown('**Root Mean Squared Error**'):
     
     # Portrait Plot
     predictions_test['RMSE']= [50 if i> 50 else i for i in predictions_test['RMSE']]
     plt.figure(figsize= (3, 9))
     sns.set_theme(style= 'whitegrid')
     ax2= sns.barplot(y= predictions_test.index, x= 'RMSE', data= predictions_test)
-  st.markdown(imagedownload(plt, 'plot-rmse-tall.pdf'), unsafe_allow_html= True)
+  st.markdown(imagedownload(plt, 'RMSE Plot [Portrait]'), unsafe_allow_html= True)
   
     # Landscape Plot
   plt.figure(figsize= (9, 3))
@@ -81,7 +81,7 @@ def build_model(df):
   ax2= sns.barplot(x= predictions_test.index, y='RMSE', data= predictions_test)
   plt.xticks(rotation= 90)
   st.pyplot(plt)
-  st.markdown(imagedownload(plt, 'plot-rmse-wide.pdf'), unsafe_allow_html= True)
+  st.markdown(imagedownload(plt, 'RMSE Plot [Landscape]'), unsafe_allow_html= True)
   
   with st.markdown('**Processing Time**'):
     
@@ -90,7 +90,7 @@ def build_model(df):
     plt.figure(figsize= (3, 9))
     sns.set_theme(style= 'whitegrid')
     ax3= sns.barplot(y= predictions_test.index, x= 'Time Taken', data= predictions_test)
-  st.markdown(imagedownload(plt, 'plot-processing-time-tall.pdf'), unsafe_allow_html= True)
+  st.markdown(imagedownload(plt, 'Processing-Time Plot [Portrait]'), unsafe_allow_html= True)
   
     # Landscape Plot
   plt.figure(figsize= (9, 3))
@@ -98,14 +98,14 @@ def build_model(df):
   ax3= sns.barplot(x= predictions_test.index, y= 'Time Taken', data= predictions_test)  
   plt.xticks(rotation= 90)
   st.pyplot(plt)
-  st.markdown(imagedownload(plt, 'plot-processing-time-wide.pdf'), unsafe_allow_html= True)
+  st.markdown(imagedownload(plt, 'Processing-Time Plot [Landscape]'), unsafe_allow_html= True)
 
   
 # Download data as CSV
 def filedownload(df, filename):
   csv= df.to_csv(index= False)
   b64= base64.b64encode(csv.encode()).decode() # string <-> byte conversion
-  href= f'<a href="data:file/csv;base64,{b64}" download= {filename}>Download {filename} File </a>'
+  href= f'<a href="data:file/csv;base64,{b64}" download= {filename}>Download {filename} </a>'
   return href
 
 
@@ -115,14 +115,16 @@ def imagedownload(plt, filename):
   plt.savefig(s, format= 'pdf', bbox_inches= 'tight')
   plt.close()
   b64= base64.b64encode(s.getvalue()).decode() # string <-> byte conversion
-  href= f'<a href="data:image/png;base64,{b64}" download= {filename}>Download {filename} File </a>'
+  href= f'<a href="data:image/png;base64,{b64}" download= {filename}>Download {filename} </a>'
   return href
 
 
 st.write("""
-         # AutoML Web Application
+         # Automated Machine Learning~ AutoML
          
-         Input a dataset and discover the best Supervised Learning Algorithm for your use case!      
+         Input a dataset and discover the best Supervised Learning Algorithm for your use case!   
+         
+         📝: Large datasets require a couple of minutes to build on.  
          """)
 
 
@@ -142,23 +144,24 @@ st.subheader('1. Dataset')
 
 if uploaded_file is not None:
   df= pd.read_csv(uploaded_file)
-  st.markdown('**1.1 DataFrame Peek**')
+  st.markdown('**1.1 Peek**')
   st.write(df)
   build_model(df)
 else:
   st.info('Use the sidebar to upload CSV files.')
-  if st.button('Use Example Dataset instead'):
+  if st.button('Example Input File'):
     
     # Boston Housing Data
     boston= load_boston()
-    X= pd.DataFrame(boston.data, columns= boston.feature_names)
-    Y= pd.Series(boston.target, name= 'response')
+    X= pd.DataFrame(boston.data, columns= boston.feature_names).loc[:250]
+    Y= pd.Series(boston.target, name= 'response').loc[:250]
     df= pd.concat([X, Y], axis= 1)
     
     st.markdown('The Boston Housing Dataset is used as an example.')
     st.write(df.head(5)) 
     
     build_model(df)
+
 
       
     
